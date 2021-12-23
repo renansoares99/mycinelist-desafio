0e7fb3e563dadee175ca9e1fe5fb8f6b5a9c76bd
Renan Barbosa <renansoaresarcanjo283@gmail.com>
2021-12-23 19:28:43 -0300

    Inicio do Projeto MyCineList

---
+++ src/main/java/br/com/mycinelist/MycinelistApplication.java
@@ -0,0 +1,13 @@
+package br.com.mycinelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycinelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycinelistApplication.class, args);
+	}
+
+}
